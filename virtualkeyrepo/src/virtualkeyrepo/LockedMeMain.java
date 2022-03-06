4e13c121ea351d4787545b103eb111a26c962f31
Niharika Kottha <niharika.kotha1@gmail.com>
2022-03-06 22:06:37 +0530

    sample

---
+++ virtualkeyrepo/src/virtualkeyrepo/LockedMeMain.java
@@ -0,0 +1,18 @@
+package virtualkeyrepo;
+
+
+
+public class LockedMeMain {
+
+public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		MenuOptions.printWelcomeScreen("LockedMe", "NIHARIKA KOTTHA");
+		
+		HandleOptions.handleWelcomeScreenInput();
+	}
+
+	
+}
